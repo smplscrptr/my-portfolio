bfa014e541a51e0ada59b737bae9b0d65cb73e01
Bennett W <xbennett.woods@gmail.com>
2025-09-07 17:01:44 -0500

    last push before deployment test

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: 'https://your-domain.com',
+  site: 'https://bennettwoods.dev',
   integrations: [sitemap()],
   vite: { plugins: [tailwindcss()] },
 })
